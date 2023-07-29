25edfadb596506edb77c57f998f7604a4ee7b2e0
chithuganesan <chithresu@secqureone.com>
2023-07-29 11:30:55 +0530

    update issues

--- src/Traits/FilterSortingSearchable.php
+++ src/Traits/FilterSortingSearchable.php
@@ -19,26 +19,29 @@ trait FilterSortingSearchable
     public function scopeFilterable(Builder $query) {
 
         $params = app('request')->except(['page', '_token', 'search']);
-        if(is_array($this->filterable)) {
+
             foreach($params as $column_key => $column_value) {
-               
+                
+                if(in_array($column_key, $this->filterable ?? [])) {
+                    $array_multipleValue = explode(',', $column_value);
+                    if(count($array_multipleValue) != 1) {
+                        $query?->OrWhereBetween($column_key, $array_multipleValue);
+                    } else {
+                        $query?->OrWhere($column_key, 'LIKE', '%' . $column_value . '%');
+                    }
+
+             }
+
+             if(in_array($column_key, $this->customfilterable ?? [])) {
                 try {
                     $namespace = app()->getNamespace() . "CustomFilter\\CustomFilterTrait";
                     $query = $namespace::customFilter($query, $column_key, $column_value);
                 } catch (\Throwable $th) {
                     $query = $query;
                 }
-                
-                if(in_array($column_key, $this->filterable)) {
-                    $array_multipleValue = explode(',', $column_value);
-                    if(count($array_multipleValue) != 1) {
-                        $query->OrWhereBetween($column_key, $array_multipleValue);
-                    } else {
-                        $query->OrWhere($column_key, 'LIKE', '%' . $column_value . '%');
-                    }
              }
+
         }
-    }
         return $query;
     }
 
